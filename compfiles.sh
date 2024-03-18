86f5c86562fc4d82b03ffec24f330d73c65af48a
bblackwell <bill.blackwell@verizon.net>
2024-03-18 15:07:33 +0000

    Added commands to compress files in Sxfer directory using zip. The script will remove the source files (zip option -m).

--- compfiles.sh
+++ compfiles.sh
@@ -1,4 +1,14 @@
-#!/bin/bash
+#!/bin/bash -e
 # 00:10 crontab job to compress all files to be xferred to WWVARC repo 
 date
-echo Hello 12:00!
+echo 'Grape 2 compress files shell script'
+cd /home/pi/G2DATA/Sxfer
+
+DATE=`date +%Y-%m-%d --date="1 day ago"`
+NODE=`cat /home/pi/PSWS/Sinfo/NodeNum.txt`
+ZIPFILE=${DATE}T000000Z_${NODE}
+
+time /usr/bin/zip -m ${ZIPFILE} *
+
+echo Compress files script ended
+
