b0190329a7acc5e68b62b82cf69acd5adb91473f
bblackwell <bill.blackwell@verizon.net>
2024-03-22 16:40:51 +0000

    Don't recompress files left in the xfer directory

--- compfiles.sh
+++ compfiles.sh
@@ -1,18 +1,18 @@
 #!/bin/bash -e
-# 00:10 crontab job to compress all files to be xferred to WWVARC repo 
+# 00:10 crontab job to compress all files to be xferred to WWVARC repo
 date
 echo 'Grape 2 compress files shell script'
 cd /home/pi/G2DATA/Sxfer
 
 DATE=`date +%Y-%m-%d --date="1 day ago"`
 NODE=`cat /home/pi/PSWS/Sinfo/NodeNum.txt`
-ZIPFILE=${DATE}T000000Z_${NODE}
+PATTERN=${DATE}T000000Z_${NODE}
 
 # zip all daily files for transfer
-time /usr/bin/zip -m ${ZIPFILE} *
+time /usr/bin/zip -m ${PATTERN} ${PATTERN}*
 
 # gzip daily dc log file for long term storage
-/usr/bin/gzip /home/pi/G2DATA/Slogs/${ZIPFILE}_DC.log
+/usr/bin/gzip /home/pi/G2DATA/Slogs/${PATTERN}_DC.log
 
 echo Compress files script ended
 
