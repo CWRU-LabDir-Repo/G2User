ee972ead17c3046452e03f62e7e06475f360bf98
John C Gibbons <jcg66@case.edu>
2024-04-02 12:59:10 -0400

    fix crontab job and repo

--- crontab2345.sh
+++ crontab2345.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 # 23:45 crontab job for root 
 date
-echo crontab 23:45 job - pull in repo exact copy
+echo crontab 23:45 job - pull in repo exact copy and fix crontab job for g2plot
 cd /home/pi/G2User/
 git fetch
 git reset --hard HEAD
 git merge '@{u}'
+# fix crontab job
+/home/pi/G2User/g2plot-crontab-fix.sh
+
