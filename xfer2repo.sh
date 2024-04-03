26ba1e8fa5cad4c3ef9265885a33ef19692abd8a
John C Gibbons <jcg66@case.edu>
2024-04-03 11:34:13 -0400

    fixed sudo permission needed to delete files

--- xfer2repo.sh
+++ xfer2repo.sh
@@ -15,7 +15,7 @@ cat /home/pi/PSWS/Stemp/go2repo
 echo 'Attempting xfer to repo...'
 if (< /home/pi/PSWS/Stemp/go2repo xargs -I %  curl -v -u "grape@wwvarc.org:5F3gjdEKEt" -T "{%}" ftp://208.109.41.230/);
 then
-    echo 'Files transferred ok - removing them from ~/G2DATA/Sxfer/'; rm /home/pi/G2DATA/Sxfer/*;
+    echo 'Files transferred ok - removing them from ~/G2DATA/Sxfer/'; sudo rm /home/pi/G2DATA/Sxfer/*;
 else
     echo 'File xfer failed - leaving files in ~/G2DATA/SXfer/ for next try tomorrow';
 fi
