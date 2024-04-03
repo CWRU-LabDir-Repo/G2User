af9a61312fb950cf37de1465b365da8f64d49a79
JohnCGibbons <jcg66@case.edu>
2024-04-02 21:14:44 -0400

    added making repo pull allowed by root

--- crontab2345.sh
+++ crontab2345.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # 23:45 crontab job for root 
 date
-echo crontab 23:45 job - pull in repo exact copy and fix crontab job for g2plot
+echo crontab 23:45 job - pull in repo exact copy and fix crontab running as root
+git config --global --add safe.directory /home/pi/G2User
 cd /home/pi/G2User/
 git fetch
 git reset --hard HEAD
