f5c22a3c6e037839fca1134856d128b02642249d
bblackwell <bill.blackwell@verizon.net>
2024-04-21 07:29:58 +0000

    Edit crontabs to move g2plot.sh job from sudo crontab to pi crontab. Also add the autostart config to start G2console (with -r) at pi user login.

--- crontab2345.sh
+++ crontab2345.sh
@@ -2,3 +2,11 @@
 # 23:45 crontab job for root 
 date
 echo crontab 23:45 job - Hello!
+
+echo Fix crontabs for g2plot
+/home/pi/G2User/g2plot-crontab-fix.sh
+
+echo Installing autostart
+cp -p /home/pi/G2User/autostart /home/pi/.config/lxsession/LXDE-pi
+
+echo crontab 23:45 job done
--- g2plot-crontab-fix.sh
+++ g2plot-crontab-fix.sh
@@ -1,25 +1,48 @@
-#!/bin/bash -e
-# The "-e" option will cause this script to exit on any error.
-# Job to fix crontab entry for g2plot.sh
+#!/bin/bash
+# Job to fix crontab entries for g2plot.sh
+# OK to run more than once.
+# AB1XB 24-04-21
+
 date
-echo 'Grape 2 fix g2plot.sh crontab entry'
+echo 'Grape 2 fix crontab entries for g2plot.sh'
 
 # work in Stemp
 push=`pwd`
 cd /home/pi/PSWS/Stemp
 
-# save existing root crontab to a file
+# save existing pi and root crontabs to files
+echo "getting existing crontab files"
+crontab -u pi -l > crontab-pi
 sudo crontab -l > crontab-root
 
-# edit crontab file to remove the "/bin/python3 " string from the g2plot.sh entry
-sed 's/\/bin\/python3 \/home\/pi\/G2User\/g2plot.sh/\/home\/pi\/G2User\/g2plot.sh/' < crontab-root > crontab-root-fixed
+# edit root crontab file to delete the 2 lines for the g2plot.sh entry
+echo "editing crontab-root"
+sed '/00:03 Creates plots/,/03 00 * * */d' crontab-root > crontab-root-fixed
 
-# replace crontab with the fixed file
+# replace root crontab with the fixed file
+echo "replacing crontab-root"
 sudo crontab crontab-root-fixed
 
+# search for an existing g2plot entry in the pi crontab so we add it only once
+echo "searching for g2plot.sh in crontab-pi"
+grep -q "g2plot.sh" crontab-pi
+if [ $? == 1 ]
+then
+# entry not found, edit pi crontab file to add the 2 lines for the g2plot.sh entry
+echo "editing crontab-pi"
+sed '/# 23:30/i \
+# 00:03 Creates plots of 3 radio files data\
+03 00 * * * /home/pi/G2User/g2plot.sh >/home/pi/PSWS/Sstat/g2plot.stat 2>&1' crontab-pi > crontab-pi-fixed
+# replace pi crontab with the fixed file
+echo "replacing crontab-pi"
+crontab -u pi crontab-pi-fixed
+else
+    echo "g2plot.sh already found in crontab-pi"
+fi
+
 # clean up
+sudo rm -f crontab-pi crontab-pi-fixed
 sudo rm -f crontab-root crontab-root-fixed
 cd $push
 
-echo 'Fixed g2plot.sh crontab entry'
-
+echo 'Fixed crontab entries for g2plot.sh'
