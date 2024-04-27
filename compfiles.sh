a1186fd864350547ab8daa8c614379587e00dd29
bblackwell <bill.blackwell@verizon.net>
2024-04-27 03:57:08 +0000

    Compress logs to separate file

--- compfiles.sh
+++ compfiles.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -e
+#!/bin/bash
 # 00:10 crontab job to compress all files to be xferred to WWVARC repo
 date
 echo 'Grape 2 compress files shell script'
@@ -8,11 +8,16 @@ DATE=`date +%Y-%m-%d --date="1 day ago"`
 NODE=`cat /home/pi/PSWS/Sinfo/NodeNum.txt`
 PATTERN=${DATE}T000000Z_${NODE}
 
-# zip all daily files for transfer
-time /usr/bin/zip -m ${PATTERN} ${PATTERN}*
-
 # gzip daily dc log file for long term storage
 /usr/bin/gzip /home/pi/G2DATA/Slogs/${PATTERN}_DC.log
+
+# zip all daily csv files for transfer
+time /usr/bin/zip -m ${PATTERN} ${PATTERN}*.csv
+
+# zip all daily log files for transfer
+# compfiles.stat will be truncated because we are currently redirecting to it.
+time /usr/bin/zip ${PATTERN}_logs ${PATTERN}*.log /home/pi/PSWS/Sstat/*.stat
+/usr/bin/rm -f ${PATTERN}*.log
 
 echo Compress files script ended
 
