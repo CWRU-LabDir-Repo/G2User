96d7e6b395588d73865f87883a169902dd112a06
John C Gibbons <jcg66@case.edu>
2024-05-14 08:55:58 -0400

    removed creation of g2plot.stat file from g2plot.sh

--- g2plot.sh
+++ g2plot.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 date
 echo 'Grape 2 plot files shell script '
-/bin/find /home/pi/G2DATA/SdataR* -type f -name "$(date -d '1 day ago' +'%Y-%m-%d')*" | /bin/python3 /home/pi/G2User/g2plot.py >/home/pi/PSWS/Sstat/g2plot.stat 2>&1
+/bin/find /home/pi/G2DATA/SdataR* -type f -name "$(date -d '1 day ago' +'%Y-%m-%d')*" | /bin/python3 /home/pi/G2User/g2plot.py
