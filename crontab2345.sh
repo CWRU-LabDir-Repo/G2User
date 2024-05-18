8d07f6fe1ab3f9d5ed00f46bbbbb7e9e2eb30d61
bblackwell <bill.blackwell@verizon.net>
2024-05-18 03:58:02 +0000

    Examine root and pi crontabs

--- crontab2345.sh
+++ crontab2345.sh
@@ -2,4 +2,6 @@
 # 23:45 crontab job for root 
 date
 echo crontab 23:45 job - Hello!
+crontab -l > /home/pi/PSWS/Sstat/crontab-pi.stat 2>&1
+sudo crontab -l > /home/pi/PSWS/Sstat/crontab-root.stat 2>&1
 echo crontab 23:45 job done
