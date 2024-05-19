35b61d87d97438cc0d7295a62c190d85c6179109
bblackwell <bill.blackwell@verizon.net>
2024-05-19 04:27:52 +0000

    Add pi user for crontab -l

--- crontab2345.sh
+++ crontab2345.sh
@@ -2,6 +2,6 @@
 # 23:45 crontab job for root 
 date
 echo crontab 23:45 job - Hello!
-crontab -l > /home/pi/PSWS/Sstat/crontab-pi.stat 2>&1
+crontab -u pi -l > /home/pi/PSWS/Sstat/crontab-pi.stat 2>&1
 sudo crontab -l > /home/pi/PSWS/Sstat/crontab-root.stat 2>&1
 echo crontab 23:45 job done
