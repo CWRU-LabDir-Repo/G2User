679a5d1b71ba947b0790262f96aac5d6d126f08b
John C Gibbons <jcg66@case.edu>
2024-05-20 13:27:21 -0400

    Adde install of scipy matplotlib as user pi

--- crontab2345.sh
+++ crontab2345.sh
@@ -2,6 +2,8 @@
 # 23:45 crontab job for root 
 date
 echo crontab 23:45 job - Hello!
+sudo -u pi pip3 install scipy
+sudo -u pi pip3 install matplotlib
 crontab -u pi -l > /home/pi/PSWS/Sstat/crontab-pi.stat 2>&1
 sudo crontab -l > /home/pi/PSWS/Sstat/crontab-root.stat 2>&1
 echo crontab 23:45 job done
