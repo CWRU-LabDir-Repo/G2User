16b567c81ce06e74dfb7333a9cf4c4aef9ce4d81
bblackwell <bill.blackwell@verizon.net>
2024-05-20 23:01:17 +0000

    Examine /boot/config.txt

--- crontab2345.sh
+++ crontab2345.sh
@@ -6,4 +6,5 @@ sudo -u pi pip3 install scipy
 sudo -u pi pip3 install matplotlib
 crontab -u pi -l > /home/pi/PSWS/Sstat/crontab-pi.stat 2>&1
 sudo crontab -l > /home/pi/PSWS/Sstat/crontab-root.stat 2>&1
+cp /boot/cmdline.txt /home/pi/PSWS/Sstat/boot-cmdline-txt.stat
 echo crontab 23:45 job done
