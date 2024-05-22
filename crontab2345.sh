eb7937433890576e7f8f1512c1465451a3e056fd
bblackwell <bill.blackwell@verizon.net>
2024-05-22 02:52:26 +0000

    Disabled commands to install scipy and matplotlib

--- crontab2345.sh
+++ crontab2345.sh
@@ -2,8 +2,8 @@
 # 23:45 crontab job for root 
 date
 echo crontab 23:45 job - Hello!
-sudo -u pi pip3 install scipy
-sudo -u pi pip3 install matplotlib
+#sudo -u pi pip3 install scipy
+#sudo -u pi pip3 install matplotlib
 crontab -u pi -l > /home/pi/PSWS/Sstat/crontab-pi.stat 2>&1
 sudo crontab -l > /home/pi/PSWS/Sstat/crontab-root.stat 2>&1
 cp /boot/cmdline.txt /home/pi/PSWS/Sstat/boot-cmdline-txt.stat
