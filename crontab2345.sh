6e458ad87b6af409273f3350f749fc73561cc037
bblackwell <bill.blackwell@verizon.net>
2024-05-23 20:00:17 +0000

    Remove g2plot.stat with root permissions

--- crontab2345.sh
+++ crontab2345.sh
@@ -4,6 +4,7 @@ date
 echo crontab 23:45 job - Hello!
 #sudo -u pi pip3 install scipy
 #sudo -u pi pip3 install matplotlib
+/usr/bin/rm -f /home/pi/PSWS/Sstat/g2plot.stat
 crontab -u pi -l > /home/pi/PSWS/Sstat/crontab-pi.stat 2>&1
 sudo crontab -l > /home/pi/PSWS/Sstat/crontab-root.stat 2>&1
 cp /boot/cmdline.txt /home/pi/PSWS/Sstat/boot-cmdline-txt.stat
