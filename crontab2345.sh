8a31a42d1c94f32a0bbda2b9bc4f7b47a549c0dc
bblackwell <bill.blackwell@verizon.net>
2024-05-24 17:22:16 +0000

    Fix g2plot crontab entries for node 1014

--- crontab2345.sh
+++ crontab2345.sh
@@ -4,6 +4,12 @@ date
 echo crontab 23:45 job - Hello!
 #sudo -u pi pip3 install scipy
 #sudo -u pi pip3 install matplotlib
+NODE=`cat /home/pi/PSWS/Sinfo/NodeNum.txt`
+if [ $NODE == "N0001014" ]
+then
+    echo "Running g2plot-crontab-fix.sh for node ${NODE}"
+    /home/pi/G2User/g2plot-crontab-fix.sh
+fi
 /usr/bin/rm -f /home/pi/PSWS/Sstat/g2plot.stat
 crontab -u pi -l > /home/pi/PSWS/Sstat/crontab-pi.stat 2>&1
 sudo crontab -l > /home/pi/PSWS/Sstat/crontab-root.stat 2>&1
