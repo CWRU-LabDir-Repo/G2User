980e23d0e85789191a9a22b27ae555cbb4a9815b
bblackwell <bill.blackwell@verizon.net>
2024-08-05 03:27:41 +0000

    Auto-plot magnetometer data if it exists.

--- g2plot.sh
+++ g2plot.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo 'Grape 2 plot files shell script'
-/bin/find /home/pi/G2DATA/SdataR* -type f -name "$(date -d '1 day ago' +'%Y-%m-%d')*" | /bin/python3 /home/pi/G2User/g2plot.py
+/bin/find /home/pi/G2DATA/SdataR* /home/pi/G2DATA/Smagtmp -type f -name "$(date -d '1 day ago' +'%Y-%m-%d')*" | /bin/python3 /home/pi/G2User/g2plot.py
