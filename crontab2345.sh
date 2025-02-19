29a52936d42a94cb03832e3ef34a1a26c0b72b4f
bblackwell <bill.blackwell@verizon.net>
2025-02-19 22:18:30 +0000

    Added some logging of node file information prior to HDD exchange and G2DATA link modification.

--- crontab2345.sh
+++ crontab2345.sh
@@ -11,6 +11,19 @@ echo crontab 23:45 job - Hello!
 #    /home/pi/G2User/g2plot-crontab-fix-1024.sh
 #fi
 #/usr/bin/rm -f /home/pi/PSWS/Sstat/g2plot.stat
+
+# Get some info before we change G2DATA to a link.
+echo G2DATA file info:
+file /home/pi/G2DATA
+echo Disk usage:
+df
+echo autostart:
+cat /home/pi/G2User/autostart
+echo /G2User contents:
+ls -al /home/pi/G2User
+echo /etc/fstab:
+cat /etc/fstab
+
 crontab -u pi -l > /home/pi/PSWS/Sstat/crontab-pi.stat 2>&1
 sudo crontab -l > /home/pi/PSWS/Sstat/crontab-root.stat 2>&1
 cp /boot/cmdline.txt /home/pi/PSWS/Sstat/boot-cmdline-txt.stat
