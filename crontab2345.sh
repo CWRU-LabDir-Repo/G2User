cf17ade2db769ec950d43fec6e54267f4dbfbb03
bblackwell <bill.blackwell@verizon.net>
2025-03-19 02:57:29 +0000

    Disable some station info logging.

--- crontab2345.sh
+++ crontab2345.sh
@@ -2,40 +2,40 @@
 # 23:45 crontab job for root 
 date
 echo crontab 23:45 job - Hello!
-NODE=`cat /home/pi/PSWS/Sinfo/NodeNum.txt`
+#NODE=`cat /home/pi/PSWS/Sinfo/NodeNum.txt`
 #if [ $NODE == "N0001002" ]
 #then
-echo "Running git-crontab-fix.sh for node ${NODE}"
-/home/pi/G2User/git-crontab-fix.sh
+#echo "Running git-crontab-fix.sh for node ${NODE}"
+#/home/pi/G2User/git-crontab-fix.sh
 #fi
 
 # Add the preswap alias
-grep preswap /home/pi/.bashrc
-if [ $? != 0 ]
-then
-    sudo -u pi sed -i -e '$a \
-\
-alias preswap="sudo /home/pi/G2User/preswap.sh 2>&1 | sudo tee -a /home/pi/preswap.stat; sudo mv /home/pi/preswap.stat /home/pi/PSWS/Sstat"' /home/pi/.bashrc
-fi
+#grep preswap /home/pi/.bashrc
+#if [ $? != 0 ]
+#then
+#    sudo -u pi sed -i -e '$a \
+#\
+#alias preswap="sudo /home/pi/G2User/preswap.sh 2>&1 | sudo tee -a /home/pi/preswap.stat; sudo mv /home/pi/preswap.stat /home/pi/PSWS/Sstat"' /home/pi/.bashrc
+#fi
 
 # Activate the new autostart script
-sudo -u pi cp -p /home/pi/G2User/autostart /home/pi/.config/lxsession/LXDE-pi
+#sudo -u pi cp -p /home/pi/G2User/autostart /home/pi/.config/lxsession/LXDE-pi
 
 # Get some info before we change G2DATA to a link.
-echo G2DATA file info:
-file /home/pi/G2DATA
+#echo G2DATA file info:
+#file /home/pi/G2DATA
 echo Disk usage
 /usr/bin/df -l
-echo autostart installed
-cat /home/pi/.config/lxsession/LXDE-pi/autostart
-echo /G2User contents
-ls -al /home/pi/G2User
-echo /etc/fstab
-cat /etc/fstab
-echo .bashrc
-cat /home/pi/.bashrc
+#echo autostart installed
+#cat /home/pi/.config/lxsession/LXDE-pi/autostart
+#echo /G2User contents
+#ls -al /home/pi/G2User
+#echo /etc/fstab
+#cat /etc/fstab
+#echo .bashrc
+#cat /home/pi/.bashrc
 
 crontab -u pi -l > /home/pi/PSWS/Sstat/crontab-pi.stat 2>&1
 sudo crontab -l > /home/pi/PSWS/Sstat/crontab-root.stat 2>&1
-cp /boot/cmdline.txt /home/pi/PSWS/Sstat/boot-cmdline-txt.stat
+#cp /boot/cmdline.txt /home/pi/PSWS/Sstat/boot-cmdline-txt.stat
 echo crontab 23:45 job done
