87838ab3ccbed058ef653f40efba8585011da969
bblackwell <bill.blackwell@verizon.net>
2025-04-02 03:15:38 +0000

    Changed info tracking for HDD swap

--- crontab2345.sh
+++ crontab2345.sh
@@ -2,7 +2,8 @@
 # 23:45 crontab job for root 
 date
 echo crontab 23:45 job - Hello!
-#NODE=`cat /home/pi/PSWS/Sinfo/NodeNum.txt`
+NODE=`cat /home/pi/PSWS/Sinfo/NodeNum.txt`
+echo NODE=$NODE
 #if [ $NODE == "N0001002" ]
 #then
 #echo "Running git-crontab-fix.sh for node ${NODE}"
@@ -24,16 +25,20 @@ echo crontab 23:45 job - Hello!
 # Get some info before we change G2DATA to a link.
 #echo G2DATA file info:
 #file /home/pi/G2DATA
-echo Disk usage
-/usr/bin/df -l
+#echo Disk usage
+#/usr/bin/df -l
 #echo autostart installed
 #cat /home/pi/.config/lxsession/LXDE-pi/autostart
 #echo /G2User contents
 #ls -al /home/pi/G2User
 #echo /etc/fstab
 #cat /etc/fstab
+echo G2DATA entry in /etc/fstab:
+grep G2DATA /etc/fstab
 #echo .bashrc
 #cat /home/pi/.bashrc
+echo External drives:
+/usr/bin/df | grep "/dev/sd"
 
 crontab -u pi -l > /home/pi/PSWS/Sstat/crontab-pi.stat 2>&1
 sudo crontab -l > /home/pi/PSWS/Sstat/crontab-root.stat 2>&1
