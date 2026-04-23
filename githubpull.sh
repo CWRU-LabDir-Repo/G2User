abe71d456755ac29e0f5af3688081a4038427ac4
bblackwell <bill.blackwell@verizon.net>
2026-04-23 21:43:10 +0000

    githubpull.sh restore file modification times

--- githubpull.sh
+++ githubpull.sh
@@ -5,3 +5,14 @@ cd /home/pi/G2User/
 git fetch
 git reset --hard HEAD
 git merge '@{u}'
+
+# restore file modification times
+git restore-mtime
+code=$?
+if [ $code != 0 ]
+then
+    echo "git-restore-mtime not installed, installing now"
+    sudo apt install git-restore-mtime
+    git restore-mtime
+fi
+
