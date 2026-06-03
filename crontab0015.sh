8cea508f2d005e0774a6950368c793f0e004548a
bblackwell <bill.blackwell@verizon.net>
2026-06-03 03:08:38 +0000

    [no restart] removed mesh agent install for N0001022

--- crontab0015.sh
+++ crontab0015.sh
@@ -6,12 +6,11 @@ echo Crontab 00:15 job
 echo
 
 # Install meshagent for selected nodes
-NODE=`cat /home/pi/PSWS/Sinfo/NodeNum.txt`
-if [ $NODE == "N0001022" ]
-then
-    echo "Installing meshagent for Node ${NODE}"
-    cd /home/pi/G2User/mesh
-    sudo ./mesh_agent_install.sh > /home/pi/PSWS/Sstat/mesh_agent_install.stat 2>&1
-fi
-
+#NODE=`cat /home/pi/PSWS/Sinfo/NodeNum.txt`
+#if [ $NODE == "N0001022" ]
+#then
+#    echo "Installing meshagent for Node ${NODE}"
+#    cd /home/pi/G2User/mesh
+#    sudo ./mesh_agent_install.sh > /home/pi/PSWS/Sstat/mesh_agent_install.stat 2>&1
+#fi
 
