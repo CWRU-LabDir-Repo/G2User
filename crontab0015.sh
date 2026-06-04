009d2a5917e345b2b27248c73d35e4284a7a8b97
bblackwell <bill.blackwell@verizon.net>
2026-06-04 02:59:54 +0000

    [no restart] Install mesh agent on N0001020; disable magdata on N0001026

--- crontab0015.sh
+++ crontab0015.sh
@@ -5,12 +5,21 @@ date
 echo Crontab 00:15 job
 echo
 
-# Install meshagent for selected nodes
-#NODE=`cat /home/pi/PSWS/Sinfo/NodeNum.txt`
-#if [ $NODE == "N0001022" ]
-#then
-#    echo "Installing meshagent for Node ${NODE}"
-#    cd /home/pi/G2User/mesh
-#    sudo ./mesh_agent_install.sh > /home/pi/PSWS/Sstat/mesh_agent_install.stat 2>&1
-#fi
+NODE=`cat /home/pi/PSWS/Sinfo/NodeNum.txt`
 
+# Install meshagent for selected nodes
+if [ $NODE == "N0001020" ]
+then
+    echo "Installing meshagent for Node ${NODE}"
+    cd /home/pi/G2User/mesh
+    sudo ./mesh_agent_install.sh > /home/pi/PSWS/Sstat/mesh_agent_install.stat 2>&1
+fi
+
+# Disable magdata for selected node and restart console
+if [ $NODE == "N0001026" ]
+then
+    echo "Disabling magnetometer data and restarting console for Node ${NODE}"
+    cd /home/pi/PSWS/Scmd
+    mv -v magtmp _magtmp
+    touch restartcon
+fi
