1a90232d55479dc8c186d27d6a60595377a3a615
bblackwell <bill.blackwell@verizon.net>
2026-08-24 01:51:43 +0000

    Fix restart test in crontab2345.sh.

--- crontab2345.sh
+++ crontab2345.sh
@@ -134,7 +134,7 @@ grep -q "Already up to date" /home/pi/PSWS/Sstat/githubpull.stat
 UCODE=$?
 if [ $UCODE != 0 ]
 then
-    grep -q "\[no restart\]" /home/pi/PSWS/Sstat/githubpull.stat
+    git log -1 | grep "\[no restart\]"
     UCODE=$?
     if [ $UCODE != 0 ]
     then
